86515f8cecfdb0b830ea37876c4c239e39aef563
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-24 14:57:15 +0200

    add toy class

---
+++ models/Toy.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../Product.php';
+
+class Toy extends Product
+{
+    public $specifics;
+    public $size;
+
+    public function __construct($animal_type, $product_type, $price, $thumb, $specifics, $size)
+    {
+        parent::__construct($animal_type, $product_type, $price, $thumb);
+
+        $this->specifics = $specifics;
+        $this->size = $size;
+    }
+}
